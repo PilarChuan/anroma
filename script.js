4de6846cb67480e1152dcd371a70907b2ca4cdcf
pilarchuan <pnela294@gmail.com>
2023-04-12 18:56:14 -0600

    actualizado

--- script.js
+++ script.js
@@ -2,10 +2,44 @@ let iconHome = document.querySelector('#home');
 let iconCloset = document.querySelector('#closet');
 let iconSpg = document.querySelector('#spg');
 
-const elementos = document.querySelectorAll('link');
+const elementos = document.querySelectorAll('.link');
 
 // INICIO
 
 iconHome.classList.add('active');
 
 // HOME
+
+iconHome.addEventListener('click', ()=>{
+  elementos.forEach(elemento =>{
+    elemento.classList.remove('active');
+  })
+
+  iconHome.classList.toggle('active');
+})
+
+// CLOSET
+
+iconCloset.addEventListener('click', ()=>{
+
+  elementos.forEach(elemento =>{
+    elemento.classList.remove('active');
+  })
+        
+  iconCloset.classList.toggle('active');
+})
+
+// SPG
+
+iconSpg.addEventListener('click', ()=>{
+
+  elementos.forEach(elemento =>{
+    elemento.classList.remove('active');
+  })
+        
+  iconSpg.classList.toggle('active');
+})
+
+
+
+
