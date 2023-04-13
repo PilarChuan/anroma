1ed06c6787ced66fd0252fa3ca5c8a7ce96262ee
pilarchuan <pnela294@gmail.com>
2023-04-12 18:35:39 -0600

    auxilio SOS

---
+++ script.js
@@ -0,0 +1,11 @@
+let iconHome = document.querySelector('#home');
+let iconCloset = document.querySelector('#closet');
+let iconSpg = document.querySelector('#spg');
+
+const elementos = document.querySelectorAll('link');
+
+// INICIO
+
+iconHome.classList.add('active');
+
+// HOME
